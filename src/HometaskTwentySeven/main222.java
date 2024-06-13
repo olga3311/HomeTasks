62e5cd250ba6cbaf6952ca07ca985a1bbf958179
Olga.Kulaga <olga.kulaga@gmail.com>
2024-06-13 15:35:21 +0200

    hometask26

---
+++ src/HometaskTwentySeven/main222.java
@@ -0,0 +1,15 @@
+package HometaskTwentySeven;
+
+import java.util.Random;
+
+public class main222 {
+
+   // Random random = new Random();
+
+           //     for (int i = 0; i < 10; i++) {
+      //  int number = random.nextInt(101);
+      //  list.add(number);
+    }
+
+
+
--- src/HometaskTwentySix/Access.java
+++ src/HometaskTwentySix/Access.java
@@ -12,39 +12,14 @@ package HometaskTwentySix;
 //4) Самостоятельно создайте пару пользователей и попробуйте проверить, к каким сайтам у них есть доступ
 // 5*) Добавьте в enum поле, обозначающее название роли пользователя на русском языке
 public class Access implements Filter {
-    public static void main(String[] args) {
-
-
-Human person1 = new Human( "John", 20, HumanType.USER);
-Human person2 = new Human("Tom", 30, HumanType.ADMIN);
-String webAddress1 = "http://web-for-user.cinema";
-String webAddress2 = "http://web-for-proff";
-
-
-
-boolean result;
-  Access access = new Access();
-  result =  access.accessAllowed(webAddress1, person1.getType());
-        System.out.println(person1.getType().getName() + " " + result);
-  result = access.accessAllowed(webAddress2, person1.getType());
-        System.out.println(person1.getType().getName() + " " + result);
-
-        result = access.accessAllowed(webAddress2, person2.getType());
-        System.out.println(person2.getType().getName() + " " + result);
-
-    }
+    private final String allowedWebAddress = "http://web-for-user";
 
     @Override
     public boolean accessAllowed(String webAddress, HumanType person) {
-        String allowedWebAddress = "http://web-for-user";
 
-        if (person == HumanType.ADMIN) {
-            return true;
-        } else if (person == HumanType.USER) {
-            if (webAddress.contains(allowedWebAddress)) {
-                return true;
-            } else return false;
-        }
-        return false;
+
+       if (person == HumanType.ADMIN) return true;
+       if (webAddress.startsWith(allowedWebAddress)) return true;
+       return false;
     }
 }
